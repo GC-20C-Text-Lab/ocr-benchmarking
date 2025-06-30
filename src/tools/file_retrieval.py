ce417a3c3a49700ddd8a48eb565e244fd34a55eb
muhammadkhalid-03 <muhammadkhalid200314@gmail.com>
2025-06-30 16:07:20 -0500

    finished json for pipeline

--- src/tools/file_retrieval.py
+++ src/tools/file_retrieval.py
@@ -17,7 +17,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def get_doc_names(dir, keep_prefix=True, prefix_delimiter="_"):
+def get_doc_names(dir, type, keep_prefix=True, prefix_delimiter="_"):
     """
     Return a list of txt document names from `dir` without the .txt extensions.
 
@@ -26,7 +26,7 @@ def get_doc_names(dir, keep_prefix=True, prefix_delimiter="_"):
     - For example: gt_kbaa-123.txt -> kbaa-123.txt (prefix_delimiter='_')
     """
 
-    gt_paths = glob.glob(os.path.join(dir, "*.txt"))
+    gt_paths = glob.glob(os.path.join(dir, f"*.{type}"))
     logger.info("Found ground-truth txt files: %s", gt_paths)
 
     doc_names = [os.path.splitext(os.path.basename(p))[0] for p in gt_paths]
@@ -41,12 +41,15 @@ def get_doc_names(dir, keep_prefix=True, prefix_delimiter="_"):
     return doc_names
 
 
-def get_all_models(llm_root, ocr_root):
+def get_all_models(llm_root, ocr_root, ocr_llm_root, type):
     """
+    NOTE: This example is for txt files, for JSON, specify in the type parameter
     llm_root is the directory where LLM model folders are located.
     ocr_root is the directory where OCR model folders are located.
+    ocr_llm_root is the directory where OCR-LLM model folders are located.
+    type is the format of the document
 
-    Example file structure:
+    Example file structure for txt:
     - llm_root
         - gpt-4o
             - doc1.txt
@@ -77,22 +80,32 @@ def get_all_models(llm_root, ocr_root):
             m for m in os.listdir(ocr_root) if os.path.isdir(os.path.join(ocr_root, m))
         ]
 
-    all_models = [("llm-img2txt", m) for m in llm_models] + [
-        ("ocr-img2txt", m) for m in ocr_models
-    ]
+    ocr_llm_models = []
+    if os.path.isdir(ocr_llm_root):
+        ocr_llm_models = [
+            m
+            for m in os.listdir(ocr_llm_root)
+            if os.path.isdir(os.path.join(ocr_llm_root, m))
+        ]
+
+    all_models = (
+        [(f"llm-img2{type}", m) for m in llm_models]
+        + [("ocr-img2txt", m) for m in ocr_models]
+        + [(f"ocr-llm-img2{type}", m) for m in ocr_llm_models]
+    )
     # sort by model name
     all_models.sort(key=lambda x: x[1].lower())
 
     return all_models
 
 
-def get_docs(dir, doc_names, name_has_prefix=False):
+def get_docs(dir, doc_names, type, name_has_prefix=False):
     """
     Returns a 2-tuple containing
     - a dict with
         - `doc_names` as the keys
         - The contents of `dir/{doc}` as the corresponding values
-          (assuming `doc` is a key of `doc_name`).
+        (assuming `doc` is a key of `doc_name`).
             - If `name_has_prefix` is True, `doc` may be preceded by any prefix.
     - a string containing the content of all the values in the dict
         - in the order given by doc_names
@@ -104,19 +117,11 @@ def get_docs(dir, doc_names, name_has_prefix=False):
     docs = {}
     all_docs = ""
     for doc in doc_names:
-        doc_pattern = f"*{doc}.txt" if name_has_prefix else f"{doc}.txt"
-        print("Doc Pattern", doc_pattern)
+        doc_pattern = f"*{doc}.{type}" if name_has_prefix else f"{doc}.{type}"
         paths = glob.glob(os.path.join(dir, doc_pattern))
-        # if not paths:
-        #     continue
-        print(dir)
-        print(paths)
-        print("Found paths: %s", paths)
-
         with open(paths[0], "r", encoding="utf-8") as f:
             txt = f.read()
         docs[doc] = txt
-        print(docs)
         all_docs += txt + "\n"
 
     return docs, all_docs
