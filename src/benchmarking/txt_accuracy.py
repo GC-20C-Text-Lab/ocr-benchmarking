7dd3879804a5524c20b2cd2d14d02704cf18d105
Tim Yu <yuyu.tim@gmail.com>
2025-07-31 11:45:16 -0500

    Remove txt_accuracy redundancies

--- src/benchmarking/txt_accuracy.py
+++ src/benchmarking/txt_accuracy.py
@@ -240,233 +240,234 @@ def build_dataframe(title, doc_names, results_data, doc_lengths, total_doc_len):
 
     return df
 
+# The below code is redundant, please use in pipeline.ipynb to run the benchmarking instead.
 
-def get_doc_names(dir):
-    """
-    Return a list of txt document names from `dir` without the .txt extensions.
-    """
+# def get_doc_names(dir):
+#     """
+#     Return a list of txt document names from `dir` without the .txt extensions.
+#     """
 
-    gt_paths = glob.glob(os.path.join(dir, "*.txt"))
-    logger.info("Found ground-truth txt files: %s", gt_paths)
+#     gt_paths = glob.glob(os.path.join(dir, "*.txt"))
+#     logger.info("Found ground-truth txt files: %s", gt_paths)
 
-    doc_names = [os.path.splitext(os.path.basename(p))[0] for p in gt_paths]
-    logger.info("Found file names: %s", doc_names)
-    return doc_names
-
-
-def get_all_models(llm_root, ocr_root, ocr_llm_root):
-    """
-    llm_root is the directory where LLM model folders are located.
-    ocr_root is the directory where OCR model folders are located.
-    ocr_llm_root is the directory where OCR-LLM model folders are located.
-
-    Example file structure:
-    - llm_root
-        - gpt-4o
-            - doc1.txt
-            - doc2.txt
-        - gemini-2.0
-            - doc1.txt
-            - doc2.txt
-    - ocr_root
-        - pytesseract
-            - doc1.txt
-            - doc2.txt
-
-    Returns a list of 2-tuples with
-    - the model type:
-        - "llm_img2txt" for LLM models
-        - "ocr_img2txt" for OCR models
-    - the model name (found using the directory structure)
-    """
-
-    llm_models = []
-    if os.path.isdir(llm_root):
-        llm_models = [
-            m for m in os.listdir(llm_root) if os.path.isdir(os.path.join(llm_root, m))
-        ]
-    ocr_models = []
-    if os.path.isdir(ocr_root):
-        ocr_models = [
-            m for m in os.listdir(ocr_root) if os.path.isdir(os.path.join(ocr_root, m))
-        ]
-
-    ocr_llm_models = []
-    if os.path.isdir(ocr_llm_root):
-        ocr_llm_models = [
-            m
-            for m in os.listdir(ocr_llm_root)
-            if os.path.isdir(os.path.join(ocr_llm_root, m))
-        ]
-
-    all_models = (
-        [("llm-img2txt", m) for m in llm_models]
-        + [("ocr-img2txt", m) for m in ocr_models]
-        + [("ocr-llm-img2txt", m) for m in ocr_llm_models]
-    )
-    # sort by model name
-    all_models.sort(key=lambda x: x[1].lower())
-
-    return all_models
+#     doc_names = [os.path.splitext(os.path.basename(p))[0] for p in gt_paths]
+#     logger.info("Found file names: %s", doc_names)
+#     return doc_names
 
 
-def get_docs(dir, doc_names):
-    """
-    Returns a 2-tuple containing
-    - a dict with
-        - `doc_names` as the keys
-        - the contents of `dir/{key}.txt` for each key as the values.
-    - a string containing the content of all the values in the dict
-        - in the order given by doc_names
-        - each value is separated by a newline
+# def get_all_models(llm_root, ocr_root, ocr_llm_root):
+#     """
+#     llm_root is the directory where LLM model folders are located.
+#     ocr_root is the directory where OCR model folders are located.
+#     ocr_llm_root is the directory where OCR-LLM model folders are located.
 
-    Since doc_names is a list, all_docs preserves order between different directories.
-    """
+#     Example file structure:
+#     - llm_root
+#         - gpt-4o
+#             - doc1.txt
+#             - doc2.txt
+#         - gemini-2.0
+#             - doc1.txt
+#             - doc2.txt
+#     - ocr_root
+#         - pytesseract
+#             - doc1.txt
+#             - doc2.txt
 
-    docs = {}
-    all_docs = ""
+#     Returns a list of 2-tuples with
+#     - the model type:
+#         - "llm_img2txt" for LLM models
+#         - "ocr_img2txt" for OCR models
+#     - the model name (found using the directory structure)
+#     """
 
-    for doc in doc_names:
-        path = os.path.join(dir, f"{doc}.txt")
-        with open(path, "r", encoding="utf-8") as f:
-            txt = f.read()
-        docs[doc] = txt
-        all_docs += txt + "\n"
+#     llm_models = []
+#     if os.path.isdir(llm_root):
+#         llm_models = [
+#             m for m in os.listdir(llm_root) if os.path.isdir(os.path.join(llm_root, m))
+#         ]
+#     ocr_models = []
+#     if os.path.isdir(ocr_root):
+#         ocr_models = [
+#             m for m in os.listdir(ocr_root) if os.path.isdir(os.path.join(ocr_root, m))
+#         ]
 
-    return docs, all_docs
+#     ocr_llm_models = []
+#     if os.path.isdir(ocr_llm_root):
+#         ocr_llm_models = [
+#             m
+#             for m in os.listdir(ocr_llm_root)
+#             if os.path.isdir(os.path.join(ocr_llm_root, m))
+#         ]
 
+#     all_models = (
+#         [("llm-img2txt", m) for m in llm_models]
+#         + [("ocr-img2txt", m) for m in ocr_models]
+#         + [("ocr-llm-img2txt", m) for m in ocr_llm_models]
+#     )
+#     # sort by model name
+#     all_models.sort(key=lambda x: x[1].lower())
 
-def main():
-    """
-    Prerequisites:
-    - Ground truth text files located at `project_root/ground-truth/txt/gt_kbaa-pXYZ.txt`
-    - LLM/OCR transcribed files located at:
-        - for LLM transcriptions: `project_root/results/llm_img2txt/<MODEL-NAME>/<MODEL-NAME>_img_kbaa-pXYZ.txt`
-        - for OCR transcriptions: `project_root/results/ocr_img2txt/<MODEL-NAME>/<MODEL-NAME>_img_kbaa-pXYZ.txt`
-
-    The main function will:
-    - Gather all ground truth text files
-    - For each ground truth text file and for each LLM/OCR model, gather the corresponding transcription
-    - Clean all the text files (normalized and not normalized)
-    - Compute metrics for each file and model
-    - Save results in two CSV files (one for normalized, one for non-normalized)
-        - Results are saved in `project_root/benchmarking-results/txt-accuracy`
-    """
-
-    # =============
-    # Preliminaries
-    # =============
-
-    # args = parse_arguments()
-
-    logger.info("Script directory: %s", script_dir)
-    logger.info("Project root: %s", project_root)
-
-    # Ground truth
-    ground_truth_dir = os.path.join(project_root, "data", "ground-truth", "txt")
-    doc_names = get_doc_names(ground_truth_dir, keep_prefix=False)
-
-    # results/ paths
-    all_models = get_all_models(
-        os.path.join(project_root, "results", "llm-img2txt"),
-        os.path.join(project_root, "results", "ocr-img2txt"),
-        os.path.join(project_root, "results", "ocr-llm-img2txt"),
-    )
-    logger.info(f"Models found: {all_models}")
-
-    # ===========
-    # Gather files
-    # ===========
-
-    # -> Gather ground truths and put into dict:
-
-    ground_truths, ground_truths["__ALL__"] = get_docs(
-        ground_truth_dir, doc_names, name_has_prefix=True
-    )
-    doc_lengths_normalized = {
-        doc: len(clean_text_normalized(text)) for doc, text in ground_truths.items()
-    }
-    doc_lengths_nonorm = {
-        doc: len(clean_text_nonorm(text)) for doc, text in ground_truths.items()
-    }
-    total_doc_len_normalized = len(clean_text_normalized(ground_truths["__ALL__"]))
-    total_doc_len_nonorm = len(clean_text_nonorm(ground_truths["__ALL__"]))
-
-    # -> Gather each transcribed document and put into dict:
-
-    # Structure: results[model][doc]
-    results = {}
-
-    for model_type, model in all_models:
-        logger.info("Collecting results for model: %s", model)
-        model_path = os.path.join(project_root, "results", model_type, model)
-        results[model], results[model]["__ALL__"] = get_docs(
-            model_path, doc_names, name_has_prefix=True
-        )
-        logger.info("Collected results for model: %s", list(results[model].keys()))
-
-    # ===============
-    # Compute metrics
-    # ===============
-
-    normalized_results_data = {}
-    nonorm_results_data = {}
-
-    for _, model in all_models:
-        normalized_results_data[model] = {}
-        nonorm_results_data[model] = {}
-
-        logger.info("Computing metrics for model: %s", model)
-        for doc in doc_names:
-            logger.info("Computing metrics for document: %s", doc)
-            normalized_results_data[model][doc] = compute_metrics(
-                ground_truths[doc], results[model][doc], normalized=True
-            )
-            nonorm_results_data[model][doc] = compute_metrics(
-                ground_truths[doc], results[model][doc], normalized=False
-            )
-
-        normalized_results_data[model]["__ALL__"] = compute_metrics(
-            ground_truths["__ALL__"], results[model]["__ALL__"], normalized=True
-        )
-        nonorm_results_data[model]["__ALL__"] = compute_metrics(
-            ground_truths["__ALL__"], results[model]["__ALL__"], normalized=False
-        )
-
-    # Compute metrics separately for __ALL__]
-
-    # ====================
-    # Put metrics in table
-    # ====================
-
-    time = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-
-    normalized_df = build_dataframe(
-        f"normalized_{time}",
-        doc_names,
-        normalized_results_data,
-        doc_lengths_normalized,
-        total_doc_len_normalized,
-    )
-    nonorm_df = build_dataframe(
-        f"nonorm_{time}",
-        doc_names,
-        nonorm_results_data,
-        doc_lengths_nonorm,
-        total_doc_len_nonorm,
-    )
-
-    # ============
-    # Save results
-    # ============
-
-    # Default save to project_root/benchmarking-results/txt-accuracy
-    results_path = os.path.join(project_root, "benchmarking-results", "txt-accuracy")
-    if not os.path.exists(results_path):
-        os.makedirs(results_path)
-    normalized_df.to_csv(os.path.join(results_path, f"normalized_{time}.csv"))
-    nonorm_df.to_csv(os.path.join(results_path, f"nonorm_{time}.csv"))
+#     return all_models
 
 
-if __name__ == "__main__":
-    main()
+# def get_docs(dir, doc_names):
+#     """
+#     Returns a 2-tuple containing
+#     - a dict with
+#         - `doc_names` as the keys
+#         - the contents of `dir/{key}.txt` for each key as the values.
+#     - a string containing the content of all the values in the dict
+#         - in the order given by doc_names
+#         - each value is separated by a newline
+
+#     Since doc_names is a list, all_docs preserves order between different directories.
+#     """
+
+#     docs = {}
+#     all_docs = ""
+
+#     for doc in doc_names:
+#         path = os.path.join(dir, f"{doc}.txt")
+#         with open(path, "r", encoding="utf-8") as f:
+#             txt = f.read()
+#         docs[doc] = txt
+#         all_docs += txt + "\n"
+
+#     return docs, all_docs
+
+
+# def main():
+#     """
+#     Prerequisites:
+#     - Ground truth text files located at `project_root/ground-truth/txt/gt_kbaa-pXYZ.txt`
+#     - LLM/OCR transcribed files located at:
+#         - for LLM transcriptions: `project_root/results/llm_img2txt/<MODEL-NAME>/<MODEL-NAME>_img_kbaa-pXYZ.txt`
+#         - for OCR transcriptions: `project_root/results/ocr_img2txt/<MODEL-NAME>/<MODEL-NAME>_img_kbaa-pXYZ.txt`
+
+#     The main function will:
+#     - Gather all ground truth text files
+#     - For each ground truth text file and for each LLM/OCR model, gather the corresponding transcription
+#     - Clean all the text files (normalized and not normalized)
+#     - Compute metrics for each file and model
+#     - Save results in two CSV files (one for normalized, one for non-normalized)
+#         - Results are saved in `project_root/benchmarking-results/txt-accuracy`
+#     """
+
+#     # =============
+#     # Preliminaries
+#     # =============
+
+#     # args = parse_arguments()
+
+#     logger.info("Script directory: %s", script_dir)
+#     logger.info("Project root: %s", project_root)
+
+#     # Ground truth
+#     ground_truth_dir = os.path.join(project_root, "data", "ground-truth", "txt")
+#     doc_names = get_doc_names(ground_truth_dir, keep_prefix=False)
+
+#     # results/ paths
+#     all_models = get_all_models(
+#         os.path.join(project_root, "results", "llm-img2txt"),
+#         os.path.join(project_root, "results", "ocr-img2txt"),
+#         os.path.join(project_root, "results", "ocr-llm-img2txt"),
+#     )
+#     logger.info(f"Models found: {all_models}")
+
+#     # ===========
+#     # Gather files
+#     # ===========
+
+#     # -> Gather ground truths and put into dict:
+
+#     ground_truths, ground_truths["__ALL__"] = get_docs(
+#         ground_truth_dir, doc_names, name_has_prefix=True
+#     )
+#     doc_lengths_normalized = {
+#         doc: len(clean_text_normalized(text)) for doc, text in ground_truths.items()
+#     }
+#     doc_lengths_nonorm = {
+#         doc: len(clean_text_nonorm(text)) for doc, text in ground_truths.items()
+#     }
+#     total_doc_len_normalized = len(clean_text_normalized(ground_truths["__ALL__"]))
+#     total_doc_len_nonorm = len(clean_text_nonorm(ground_truths["__ALL__"]))
+
+#     # -> Gather each transcribed document and put into dict:
+
+#     # Structure: results[model][doc]
+#     results = {}
+
+#     for model_type, model in all_models:
+#         logger.info("Collecting results for model: %s", model)
+#         model_path = os.path.join(project_root, "results", model_type, model)
+#         results[model], results[model]["__ALL__"] = get_docs(
+#             model_path, doc_names, name_has_prefix=True
+#         )
+#         logger.info("Collected results for model: %s", list(results[model].keys()))
+
+#     # ===============
+#     # Compute metrics
+#     # ===============
+
+#     normalized_results_data = {}
+#     nonorm_results_data = {}
+
+#     for _, model in all_models:
+#         normalized_results_data[model] = {}
+#         nonorm_results_data[model] = {}
+
+#         logger.info("Computing metrics for model: %s", model)
+#         for doc in doc_names:
+#             logger.info("Computing metrics for document: %s", doc)
+#             normalized_results_data[model][doc] = compute_metrics(
+#                 ground_truths[doc], results[model][doc], normalized=True
+#             )
+#             nonorm_results_data[model][doc] = compute_metrics(
+#                 ground_truths[doc], results[model][doc], normalized=False
+#             )
+
+#         normalized_results_data[model]["__ALL__"] = compute_metrics(
+#             ground_truths["__ALL__"], results[model]["__ALL__"], normalized=True
+#         )
+#         nonorm_results_data[model]["__ALL__"] = compute_metrics(
+#             ground_truths["__ALL__"], results[model]["__ALL__"], normalized=False
+#         )
+
+#     # Compute metrics separately for __ALL__]
+
+#     # ====================
+#     # Put metrics in table
+#     # ====================
+
+#     time = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+
+#     normalized_df = build_dataframe(
+#         f"normalized_{time}",
+#         doc_names,
+#         normalized_results_data,
+#         doc_lengths_normalized,
+#         total_doc_len_normalized,
+#     )
+#     nonorm_df = build_dataframe(
+#         f"nonorm_{time}",
+#         doc_names,
+#         nonorm_results_data,
+#         doc_lengths_nonorm,
+#         total_doc_len_nonorm,
+#     )
+
+#     # ============
+#     # Save results
+#     # ============
+
+#     # Default save to project_root/benchmarking-results/txt-accuracy
+#     results_path = os.path.join(project_root, "benchmarking-results", "txt-accuracy")
+#     if not os.path.exists(results_path):
+#         os.makedirs(results_path)
+#     normalized_df.to_csv(os.path.join(results_path, f"normalized_{time}.csv"))
+#     nonorm_df.to_csv(os.path.join(results_path, f"nonorm_{time}.csv"))
+
+
+# if __name__ == "__main__":
+#     main()
